7279d52cfb43d80f17cb06192a2ea983812006e4
elizaveta-dudnik <e.elizaveta.d@gmail.com>
2019-12-02 23:59:35 +0300

    hw3
    01.12.19

---
+++ hw3/Program2.pas
@@ -0,0 +1,14 @@
+﻿// 2) Даны два числа. Вывести порядковый номер меньшего из них.
+
+var
+  number1, number2 : real;
+begin
+  writeln('введите два числа');
+  readln(number1, number2);
+  if number1 > number2 then 
+    writeln('порядковы номер меньшего из них равен 1');
+  if number2 > number1 then
+    writeln('порядковый номер меньшего из них равен 2');
+  if number1 = number2 then 
+    writeln('числа равны');
+end.
