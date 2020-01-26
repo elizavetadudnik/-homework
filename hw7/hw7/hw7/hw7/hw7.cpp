aa3bfd2d91365a1639f5c36f4055fdc4565bfbe1
Victoria <viky.fomina@yandex.ru>
2020-01-26 13:50:33 +0300

    fixed homework

--- hw7/hw7/hw7/hw7/hw7.cpp
+++ hw7/hw7/hw7/hw7/hw7.cpp
@@ -7,6 +7,7 @@ using namespace std;
 
 int task1();
 int task2();
+int task3();
 
  int main()
 {
@@ -16,62 +17,72 @@ int task2();
     return 0;
 }
 
-      // Дано вещественное число А и целое число N > 0. Используя один цикл, найти сумму  1 + А + А^2 + A^3 + ... + A^n.
+    // Дано вещественное число А и целое число N > 0. Используя один цикл, найти сумму  1 + А + А^2 + A^3 + ... + A^n.
+int task1()
+{
 
+	int N = 0;
+	double A = 0;
+	double powerOfA = 1;
+	double sum = 1;
 
-    {
-        int task1()
+    cout << "введите N";
+    cin >> N;
+    cout << "введите A";
+    cin >> A;
 
-        int N, i;
-        double A, PowerOfA, sum;
-        cout << "введите N";
-        cin >> N;
-        cout << "введите A";
-        cin >> A;
+	for (int i = 1; i <= N; i++)
+	{
+		powerOfA *= A; 
+		sum += powerOfA;
+	}
 
-        b = 1;
-        c = 1;
-        for (i = 1;i <= N;i++);
+    cout << sum << endl;
 
-        {PowerOfA *= A; sum += PowereOfA;}
-
-        cout << sum << endl;
-
-        retutn 0;
+    return 0;
 }
 
 
 
-    // Даны целые положительные числа N и K.Используя только операции сложения и вычитания, найти частное от деления нацело N на K, а также осттаток от этоого деления.
+// Даны целые положительные числа N и K.Используя только операции сложения и вычитания, найти частное от деления нацело N на K, а также осттаток 
+// от этоого деления.
+int task2()
+{
+	int N = 0;
+	int K = 0;
+	int count = 0;
+	cin >> K;
+	cin >> N;
 
+	while (N >= K)
+	{
+		N -= K;
+		++count;
+	}
+
+	cout << count << endl;
+
+    return 0;
+}
+
+        
+// Найти 1! + 2! + 3! + ... + N!.
+        
+int task3()
+{
+
+	int N = 0;
+	int factorial = 1;
+	int sum = 0;
+
+	cin >> N;
+
+    for (int i = 0; i < N; i++)
     {
-        int task2()
-
-        int n, past, current, count  
-        for (int i = 0; i < n; i++) 
-        {
-            std::cin >> current;
-            if (i > 0 && current < past) 
-            {
-            std::cout << current << " ";
-         
-            }
-                past = current;
-         return 0;
+		factorial *= i;
+        sum += factorial;
     }
+    cout << sum << endl;
 
-        // Найти 1! + 2! + 3! + ... + N!.
-
-        {
-            int task3()
-
-            int N, factorial, sum
-            for (int i = 0; i < N; i++)
-            {
-                i *= i;
-                count << i << endle
-                sum += 1;
-            }
-            count << sum << endle;
-            return 0;
-        }
+    return 0;
+}
