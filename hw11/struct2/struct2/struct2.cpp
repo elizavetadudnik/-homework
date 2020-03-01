f0769f8de9cb60c6a06f32bc18b7394c2749e488
elizaveta-dudnik <e.elizaveta.d@gmail.com>
2020-03-01 14:31:11 +0300

    hw11_part1

---
+++ hw11/struct2/struct2/struct2.cpp
@@ -0,0 +1,60 @@
+﻿
+
+#include <iostream>
+using namespace std;
+
+
+struct Parabola
+{
+	// метод
+	void setA(int coeffA)
+	{
+		int a;
+	}
+
+	void setB(int coeffB)
+	{
+		int b;
+	}
+
+	void getC(int coeffC)
+	{
+		int c;
+	}
+
+	void printParabola()
+	{
+		cin >> a;
+		cin >> b;
+		cin >> c;
+		cout << a << 'x * x + ' << b << 'x + ' << c << endl;
+	}
+
+	// поля
+	private:
+	int a = 1;
+	int b = 0;
+	int c = 0;
+};
+
+struct point
+{
+	bool pointOfParabola(int x, int y)
+	{
+		if (!Parabola)
+		{
+			pointOfParabola = false
+		}
+
+		pointOfParabola = true;
+	}
+
+
+
+
+int main()
+{
+	
+}
+
+
